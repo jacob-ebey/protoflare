c07738afb102919cc92ca68b4097825cf9109629
Jacob Ebey <jacob.ebey@live.com>
2025-10-01 16:05:11 -0700

    reduce storage operations

--- src/storage/firehose.ts
+++ src/storage/firehose.ts
@@ -29,7 +29,7 @@ abstract class BaseFirehoseListener<
   wantedCollections: Wanted;
 
   abstract handleMessage(
-    message: JetStreamMessage<Wanted[number]>,
+    message: JetStreamMessage<Wanted[number]>
   ): void | Promise<void>;
 
   constructor(ctx: DurableObjectState, env: Env, wantedCollections: Wanted) {
@@ -71,20 +71,17 @@ abstract class BaseFirehoseListener<
       this.websocket.addEventListener("close", (event) => {
         console.info(
           "Disconnected from websocket connection to Jetstream. Resetting DO.",
-          event,
+          event
         );
         this.ctx.abort("Reset due to disconnect");
       });
       this.websocket.addEventListener("message", (event) => {
         const message: JetStreamMessage<Wanted[number]> = JSON.parse(
-          event.data as string,
+          event.data as string
         );
 
         this.lastEventTime = message.time_us;
         // Store this in the DOs storage in case the DO is restarted.
-        this.ctx.waitUntil(
-          this.ctx.storage.put("lastEventTime", message.time_us),
-        );
 
         if (message.kind === "commit" && message.commit) {
           const uri = `lex:${message.commit.collection}`;
@@ -92,7 +89,11 @@ abstract class BaseFirehoseListener<
           const valid = lexicon
             ? lexicons.validate(uri, message.commit.record)
             : { success: false };
+
           if (valid.success) {
+            this.ctx.waitUntil(
+              this.ctx.storage.put("lastEventTime", message.time_us)
+            );
             this.handleMessage(message);
           }
         }
@@ -133,7 +134,7 @@ export class FirehoseListener extends BaseFirehoseListener<
   }
 
   override async handleMessage(
-    message: JetStreamMessage<(typeof wantedCollections)[number]>,
+    message: JetStreamMessage<(typeof wantedCollections)[number]>
   ) {
     if (message.kind === "commit" && message.commit) {
       const { $type: _, ...record } = message.commit.record;
@@ -142,11 +143,11 @@ export class FirehoseListener extends BaseFirehoseListener<
           uri: AtUri.make(
             message.did,
             message.commit.collection,
-            message.commit.rkey,
+            message.commit.rkey
           ).href,
           cid: message.commit.cid,
         },
-        record,
+        record
       );
     }
   }
